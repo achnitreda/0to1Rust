263311dbf033906e8602b55e37ee7eba4d9c7f5c
achnitreda <achnitreda@gmail.com>
2025-06-28 14:30:23 +0100

    ref_cell

---
+++ ref_cell/src/lib.rs
@@ -0,0 +1,46 @@
+use std::collections::HashMap;
+pub use std::cell::RefCell;
+pub use std::rc::Rc;
+
+pub mod messenger;
+
+pub use messenger::{Logger, Tracker};
+
+pub struct Worker {
+    pub track_value: Rc<i32>,
+    pub mapped_messages: RefCell<HashMap<String, String>>,
+    pub all_messages: RefCell<Vec<String>>,
+}
+
+impl Worker {
+    pub fn new(value: i32) -> Self {
+        Worker {
+            track_value: Rc::new(value),
+            mapped_messages: RefCell::new(HashMap::new()),
+            all_messages: RefCell::new(Vec::new()),
+        }
+    }
+}
+
+impl Logger for Worker {
+    fn warning(&self, msg: &str) {
+        let full_msg = format!("Warning: {}", msg);
+        
+        self.mapped_messages.borrow_mut().insert("Warning".to_string(), msg.to_string());
+        self.all_messages.borrow_mut().push(full_msg);
+    }
+
+    fn info(&self, msg: &str) {
+        let full_msg = format!("Info: {}", msg);
+        
+        self.mapped_messages.borrow_mut().insert("Info".to_string(), msg.to_string());
+        self.all_messages.borrow_mut().push(full_msg);
+    }
+
+    fn error(&self, msg: &str) {
+        let full_msg = format!("Error: {}", msg);
+        
+        self.mapped_messages.borrow_mut().insert("Error".to_string(), msg.to_string());
+        self.all_messages.borrow_mut().push(full_msg);
+    }
+}
---
+++ ref_cell/src/messenger.rs
@@ -0,0 +1,42 @@
+use std::rc::Rc;
+
+pub trait Logger {
+    fn warning(&self, msg: &str);
+    fn info(&self, msg: &str);
+    fn error(&self, msg: &str);
+}
+
+pub struct Tracker<'a> {
+    logger: &'a dyn Logger,
+    max: usize,
+}
+
+impl<'a> Tracker<'a> {
+    pub fn new(logger: &'a dyn Logger, max: usize) -> Self {
+        Tracker {
+            logger,
+            max,
+        }
+    }
+
+    pub fn set_value<T>(&self, rc_value: &Rc<T>) {
+        let current_count = Rc::strong_count(rc_value);
+        let percentage = (current_count * 100) / self.max;
+
+        if percentage >= 100 {
+            self.logger.error("you are over your quota!");
+        } else if percentage >= 70 {
+            self.logger.warning(&format!(
+                "you have used up over {}% of your quota! Proceeds with precaution", 
+                percentage
+            ));
+        }
+    }
+
+    pub fn peek<T>(&self, rc_value: &Rc<T>) {
+        let current_count = Rc::strong_count(rc_value);
+        let percentage = (current_count * 100) / self.max;
+        
+        self.logger.info(&format!("you are using up to {}% of your quota", percentage));
+    }
+}
